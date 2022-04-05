5a5a07bf2c82a56535931afb99f80786e5b1be23
Md Golam Murtuja <mdgolammurtuja@192.168.1.131>
2022-04-05 23:38:08 +0100

    first commit

---
+++ ui.R
@@ -0,0 +1,39 @@
+###Name : Md Golam Murtuja
+### Date: 22/01/2019
+### Project:  Data Science Capstone: The Next Word Prediction
+### Github repo : https://github.com/Murtuja07/Data_Science_Capstone
+
+suppressWarnings(library(shiny))
+suppressWarnings(library(markdown))
+shinyUI(navbarPage("Data Science Work",
+                   tabPanel("The Next Word Prediction",
+                            HTML("<strong>Author: Md Golam Murtuja</strong>"),
+                            br(),
+                            HTML("<strong>Date: 04 April 2022</strong>"),
+                            br(),
+                            # Sidebar
+                              sidebarLayout(
+                              sidebarPanel(
+                                helpText("Enter a partially complete sentence to begin the next word prediction"),
+                                textInput("inputString", "Enter a partial sentence here",value = ""),
+                                br(),
+                                br(),
+                                br(),
+                                br()
+                                ),
+                              mainPanel(
+                                  h2("Predicted Next Word"),
+                                  verbatimTextOutput("prediction"),
+                                  strong("Sentence Input:"),
+                                  tags$style(type='text/css', '#text1 {background-color: rgba(255,255,0,0.40); color: blue;}'), 
+                                  textOutput('text1'),
+                                  br()
+                                  #strong("Note:"),
+                                  #tags$style(type='text/css', '#text2 {background-color: rgba(255,255,0,0.40); color: black;}'),
+                                  #textOutput('text2')
+                              )
+                              )
+                             
+                  )
+)
+)
